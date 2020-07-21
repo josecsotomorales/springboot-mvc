8e5438e6e5213af6303f6bfcf8e3e5d27d06c4fa
Jose Carlos Soto Morales <josecsmorales@gmail.com>
2020-07-21 00:08:32 -0300

    Created basic project structure.

---
+++ src/main/java/com/jose/demo/controller/HomeController.java
@@ -0,0 +1,34 @@
+package com.jose.demo.controller;
+
+import com.jose.demo.dto.MessageForm;
+import com.jose.demo.service.MessageListService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+
+    MessageListService messageListService;
+
+    public HomeController(MessageListService messageListService) {
+        this.messageListService = messageListService;
+    }
+
+    @GetMapping({"", "/", "/home"})
+    public String getHomePage(@ModelAttribute("messageForm") MessageForm newMessage, Model model) {
+        model.addAttribute("greetings", this.messageListService.getMessages());
+        return "home";
+    }
+
+    @PostMapping("/home")
+    public String addMessage(MessageForm messageForm, Model model) {
+        messageListService.addMessage(messageForm.getText());
+        model.addAttribute("greetings", messageListService.getMessages());
+        messageForm.setText("");
+        return "home";
+    }
+
+}
---
+++ src/main/java/com/jose/demo/dto/MessageForm.java
@@ -0,0 +1,14 @@
+package com.jose.demo.dto;
+
+public class MessageForm {
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}

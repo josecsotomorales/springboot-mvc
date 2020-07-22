ad91f912c4dfaa10b9f39cbdbba25a122c592763
Jose Carlos Soto Morales <josecsmorales@gmail.com>
2020-07-21 23:20:15 -0300

    Adding chat controller.

--- src/main/java/com/jose/demo/controller/ChatController.java
+++ src/main/java/com/jose/demo/controller/ChatController.java
@@ -1,8 +1,12 @@
 package com.jose.demo.controller;
 
+import com.jose.demo.dto.ChatForm;
+import com.jose.demo.dto.ChatMessage;
 import com.jose.demo.service.ChatService;
-import com.jose.demo.service.MessageListService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ChatController {
@@ -13,6 +17,20 @@ public class ChatController {
         this.chatService = chatService;
     }
 
+    @GetMapping("/chat")
+    public String getChat(ChatMessage chatMessage, Model model) {
+        model.addAttribute("username");
+        chatService.getMessages();
+        return "chat";
+    }
 
+    @PostMapping("/chat")
+    public String addMessage(ChatForm chatForm, Model model) {
+        chatService.addMessage(chatForm.getUsername(), chatForm.getMessageText());
+        model.addAttribute("chat", chatService.getMessages());
+        chatForm.setUsername("");
+        chatForm.setMessageText("");
+        return "chat";
+    }
 
 }

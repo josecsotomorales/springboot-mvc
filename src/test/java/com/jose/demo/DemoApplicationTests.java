a6e30ebb6a69f6a9844964930e5953278205b9dd
Jose Carlos Soto Morales <josecsmorales@gmail.com>
2020-08-02 00:06:15 -0300

    Added selenium and junit tests.

--- src/test/java/com/jose/demo/DemoApplicationTests.java
+++ src/test/java/com/jose/demo/DemoApplicationTests.java
@@ -1,13 +1,70 @@
 package com.jose.demo;
 
+import com.jose.demo.model.ChatMessage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
 
+    @LocalServerPort
+    public int port;
+
+    public static WebDriver driver;
+
+    public String baseURL;
+
+    @BeforeAll
+    public static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        driver.quit();
+        driver = null;
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        baseURL = baseURL = "http://localhost:" + port;
+    }
+
     @Test
-    void contextLoads() {
+    public void testUserSignupLoginAndSubmitMessage() {
+        String username = "jose";
+        String password = "123456";
+        String messageText = "Hello!";
+
+
+        driver.get(baseURL + "/signup");
+
+        SignupPage signupPage = new SignupPage(driver);
+        signupPage.signup("Jose", "Soto", username, password);
+
+        driver.get(baseURL + "/login");
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(username, password);
+
+        ChatPage chatPage = new ChatPage(driver);
+        chatPage.sendChatMessage(messageText);
+
+        ChatMessage sentMessage = chatPage.getFirstMessage();
+
+        assertEquals(username, sentMessage.getUsername());
+        assertEquals(messageText, sentMessage.getMessage());
     }
 
 }
